3929ef9e94a4e99b1366ed622bd34170894c2552
brandonb <brandontbyfield@gmail.com>
2025-06-11 17:28:53 -0400

    Cleanup

--- frontend/src/pages/NewTeamPage/NewTeamPage.jsx
+++ frontend/src/pages/NewTeamPage/NewTeamPage.jsx
@@ -11,7 +11,8 @@ export default function NewTeamPage() {
     cost: '',
     heroes: []
   });
-  
+  // state object that holds values for a form where a user can input information about their team
+
   const [heroes, setHeroes] = useState([]);
   const [errorMsg, setErrorMsg] = useState('');
   const navigate = useNavigate();
@@ -92,33 +93,3 @@ export default function NewTeamPage() {
     </div>
   );
 }
-
-
-//   async function handleSubmit(evt) {
-//     evt.preventDefault();
-//     try {
-//       // sendRequest is expecting an object as the payload
-//       await heroService.create({ content });
-//       navigate('/heroes');
-//     } catch (err) {
-//       setErrorMsg('Assembling Team Failed');
-//     }
-//   }
-
-//   return (
-//     <>
-//       <h2>Assemble Team</h2>
-//       <form onSubmit={handleSubmit}>
-//         <label>Post Content</label>
-//         <input
-//           type="text"
-//           value={content}
-//           onChange={(evt) => setContent(evt.target.value)}
-//           required
-//         />
-//         <button type="submit">ASSEMBLE TEAM</button>
-//       </form>
-//       <p className="error-message">&nbsp;{errorMsg}</p>
-//     </>
-//   );
-// }
